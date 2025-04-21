5af6d415843dd3ca25cfaa34a7c2595991dec7be
ChangHaochen <zhcgithub@gmail.com>
2025-04-21 09:06:49 +0900

    update

--- src/polyfill.ts
+++
@@ -1,5 +0,0 @@
-import process from 'node:process'
-
-// We don't need to stub process.env like in the Pyodide version
-// because we actually want access to the local environment
-// But we'll keep this file for compatibility
